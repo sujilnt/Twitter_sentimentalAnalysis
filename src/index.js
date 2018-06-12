740dde48354709c5dd783414a334e02d6035f871
Sujil Anto <sujilnt@gmail.com>
2018-06-12 12:43:04 +0100

    with basic api setting

--- src/index.js
+++ src/index.js
@@ -1,2 +1,13 @@
+const newApiKey="810b0902cabf4fbaab29be8154f3bd93";
+const url  = `https://newsapi.org/v2/everything?q=brexit&apiKey=${newApiKey}`;
+fetch(url)
+    .then((response)=> response.json())
+    .then(function (data) {
+        console.log('Request succeeded with JSON response', data);
+    })
+    .catch(function (error) {
+        console.log('Request failed', error);
+    });
+
 let a ="successful ";
 console.log("adding a change",a );
